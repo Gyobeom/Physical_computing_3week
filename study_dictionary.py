99e8b023bd2cac1a5f13c8920eec1be121f1260d
Gyobeom <ikiscc@gmail.com>
2022-03-20 22:29:39 +0900

    2022-03-20

--- study_dictionary.py
+++ study_dictionary.py
@@ -55,5 +55,89 @@ for k, v in fruits.items():
 
 #음식 추천 프로그램 v0.1
 
+# alcohol_foods = {'맥주': '치킨',
+#                  '와인': '치즈',
+#                  '고량주': '짬뽕',
+#                  '소주': '골뱅이소면'}
+# alcohol = input('주문하실 술(맥주/와인/소주/고량주)은?')
+#
+# if alcohol in alcohol_foods.keys():
+#     print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+
+
+#음식 추천 프로그램 v0.15
+
+# alcohol_foods = {'맥주': '치킨',
+#                  '와인': '치즈',
+#                  '고량주': '짬뽕',
+#                  '소주': '골뱅이소면'}
+#
+# while True:
+#     alcohol = input('주문하실 술(맥주/와인/소주/고량주/결제)은?')
+#     if alcohol == '결제':
+#         break
+#     if alcohol in alcohol_foods.keys():
+#         print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+#     else:
+#         print('{0}는 판매하지 않습니다.메뉴에서 골라주세요'.format(alcohol))
+
+#음식 추천 프로그램 v0.3
+# import random
+#
+# alcohol_foods = {'맥주': '치킨',
+#                  '와인': '치즈',
+#                  '고량주': '짬뽕',
+#                  '소주': '골뱅이소면'}
+#
+#
+# while True:
+#     alcohol = input('주문하실 술(맥주/와인/소주/고량주/아무거나/결제)은?')
+#     if alcohol == '결제':
+#         break
+#     if alcohol in alcohol_foods.keys():
+#         print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+#     elif alcohol == '아무거나':
+#         alcohol = str(random.choice(list(alcohol_foods)))
+#         print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+#     else:
+#         print('{0}는 판매하지 않습니다.메뉴에서 골라주세요'.format(alcohol))
+
+
+alcohol_foods = {'맥주': '치킨',
+                 '와인': '치즈',
+                 '고량주': '짬뽕',
+                 '소주': '골뱅이소면'}
+
+#copy()이용시 같은 주소를 이용하지 않아 원본이 수정되지 않음
+#copy_alcohol = alcohol_foods
+
+copy_alcohol = alcohol_foods.copy()
+copy_alcohol['소주'] = '두부김치'
+print(copy_alcohol)
+
+#tuple
+#immutable 불변 튜플은 생성 후 값을 추가,수정등을 할 수 없음 상수의 리스트
+
+empty = () #튜플 소괄호 문법
+numbers = (1, -9, 7)
+print(type(empty))
+print(numbers[1])
+
+subjects = ('python','c++','english') #packing 패킹 개수와 언패킹할 개수가 똑같아야함
+# for subject in subjects:
+#     print(subject)
+
+kim,han,tom = subjects #unpacking 각각 kim,han,tom에 튜플 값이 들어감
+print(kim,han,tom)
+
+
+#패킹 언패킹
+a = input()
+b = input()
+
+a, b = (b, a)#packing과 unpacking을 동시에 수행  
+print(a,b)
+
+
 
 
